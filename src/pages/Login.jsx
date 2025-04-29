4819a20d57742175cd0120a0843b839e1ab6277e
TC023 <alonsobaez023@gmail.com>
2025-04-28 21:09:27 -0600

    pequeño avance de signin osf, implementación momentanea del login de Ray

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,62 +1,81 @@
-import React, { useContext } from "react";
-import { useState } from "react";
-import { useNavigate, Link } from "react-router-dom";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import { SessionContext } from "../Contexts/SessionContext";
+import './Login.css';
 
-export default function Login(){
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const { setSessionType } = useContext(SessionContext);
-    
-    const navigate = useNavigate();
+export default function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const { setSessionType } = useContext(SessionContext);
+  const navigate = useNavigate();
 
-    function handleUsernameChange(e) {
-        setUsername(e.target.value);
-    }
+  function handleUsernameChange(e) { setUsername(e.target.value); }
+  function handlePasswordChange(e) { setPassword(e.target.value); }
 
-    function handlePasswordChange(e){
-        setPassword(e.target.value);
-    }
+  function handleSubmit() {
+    const formInfo = new FormData();
+    formInfo.append("username", username);
+    formInfo.append("password", password);
 
-    function handleSignIn(){
-        navigate("/signin")
-    }
+    fetch("http://localhost:5000/login", {
+      method: "POST",
+      credentials: "include",
+      body: formInfo,
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+      setSessionType(data.tipo);
+      navigate('/');
+    })
+    .catch((error) => { console.log(error); });
+  }
 
-    function handleSubmit(){
-        const formInfo = new FormData();
-        formInfo.append("username", username);
-        formInfo.append("password", password);
-        console.log("handle submit")
-        fetch("http://localhost:5000/login",{
-            method: "POST",
-            credentials: "include",
-            body: formInfo,
-        })
-        .then((res) => res.json())
-        .then((data) => {
-            console.log(data);
-            console.log("succesful")
-            setSessionType(data.tipo)
-            navigate('/');
-            // window.location.reload();
-        })
-        .catch((error) => {console.log(error);})
-    }
+  return (
+    <div className="login-page">
+      {/*pt izquierda */}
+      <div className="login-left">
+        <img src="/logoservicio.png" alt="Logo Servicio" className="service-logo" />
+        <p className="service-text">¿Listo para registrar un proyecto?</p>
+      </div>
 
-    return(
-        <div className="content">
-            <div className="login">
-                <form>
-                <h1>Login</h1>
-                <label>Usuario:</label>
-                <input type="text" value={username} onChange={handleUsernameChange} />
-                <label>Contraseña:</label>
-                <input type="password" value={password} onChange={handlePasswordChange} />
-                </form>
-                <input type="submit" value="Entrar" onClick={handleSubmit} className="submit"/>
-                <button onClick={handleSignIn}> HOLA </button>
+      {/* {/pt derecha/} */}
+      <div className="login-right">
+        <div className="login-form">
+          <h1>Inicio de sesión</h1>
+
+          {/* Campo Usuario */}
+          <div className="input-group">
+            <label>Usuario:</label>
+            <div className="input-with-sprite">
+              <input
+                type="text"
+                value={username}
+                onChange={handleUsernameChange}
+                className="input-text"
+                onFocus={(e) => e.target.parentNode.querySelector('.sprite-decorativo').classList.add('sprite-jump')}
+                onBlur={(e) => e.target.parentNode.querySelector('.sprite-decorativo').classList.remove('sprite-jump')}
+              />
+              <img src="/teusr1.png" alt="Sprite decorativo" className="sprite-decorativo" />
             </div>
+          </div>
+
+          {/*contraseña */}
+          <div className="input-group">
+            <label>Contraseña:</label>
+            <input
+              type="password"
+              value={password}
+              onChange={handlePasswordChange}
+              className="input-text"
+            />
+          </div>
+
+          {/*Entrar */}
+          <input type="submit" value="Entrar" onClick={handleSubmit} className="submit" />
+          <input type="submit" value="Regístrate" onClick={() => navigate("/signin")} className="submit" />
         </div>
-    )
-    
+      </div>
+    </div>
+  );
 }
